e68267c452bfd150bce504cb478921c644ac46cf
heavenmonarch <heavenmonarc@gmail.com>
2026-07-11 19:10:48 +0100

    finished book routing

--- routes/api/v1/books.php
+++ routes/api/v1/books.php
@@ -1,1 +1,14 @@
 <?php
+use App\Http\Controllers\api\v1\Book;
+use App\Http\Controllers\api\v1\Book\BookController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::controller(BookController::class)->group(function () {
+    Route::get('/get-author-books', 'getAuthorBooks');
+    Route::post('/create-book', 'createBook');
+    Route::get('/get-book/{book}', 'getBook');
+    Route::patch('/update-book/{book}', 'updateBook');
+    Route::delete('/delete-book/{book}', 'deleteBook');
+    Route::post('/publish-book/{book}', 'publishBook');
+});
