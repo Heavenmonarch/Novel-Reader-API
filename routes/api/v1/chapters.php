3703707ad2d28595199dc79942a0480d5be25585
heavenmonarch <heavenmonarc@gmail.com>
2026-07-11 22:17:10 +0100

    Finished route file for chapters

--- routes/api/v1/chapters.php
+++ routes/api/v1/chapters.php
@@ -1,1 +1,13 @@
 <?php
+
+use App\Http\Controllers\api\v1\Book\ChapterController;
+Use Illuminate\Support\Facades\Route;
+
+
+Route::controller(ChapterController::class)->prefix('/books')->group(function () {
+    Route::get('/{book}/list-all-chapters', 'listAllChapters');
+    Route::get('/{book}/fetch-chapter/{chapter}', 'fetchChapter');
+    Route::post('/{book}/create-chapter', 'createChapter');
+    Route::patch('/{book}/update-chapter/{chapter}', 'updateChapter');
+    Route::delete('/{book}/delete-chapter/{chapter}', 'deleteChapter');
+})
